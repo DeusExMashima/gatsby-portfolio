4d749e3dc10cc52b6224cc995fa822568cb0a0f5
Mashima <52222646+zimashima@users.noreply.github.com>
2020-01-29 10:29:35 -0500

    minor fix

--- src/components/subcomponents/Skills.js
+++ src/components/subcomponents/Skills.js
@@ -1,22 +1,14 @@
 import React from 'react'
-import { useStaticQuery, graphql } from "gatsby"
+import skills from '../../data/mySkills.json'
 
 
 export default function Skills() {
-    const data = useStaticQuery(graphql`
-    {
-      allSkillsJson {
-        nodes {
-          icon
-        }
-      }
-    }
-  `)
+
   
     return (
         <div className="d-flex flex-wrap justify-content-around mt-4">
             {
-                data.allSkillsJson.nodes.map(icon=> (<SingleSkill icon={icon} />))
+                skills.map(skill=> (<SingleSkill icon={skill.icon} />))
             }
         </div>
     )
@@ -26,7 +18,7 @@ function SingleSkill(props){
     console.log(props)
     return (
         <span className="bg-dark rounded-circle">
-            <i className={`skill ${props.icon.icon} m-4`} />
+            <i className={`skill ${props.icon} m-4`} />
         </span>
     )
 }
