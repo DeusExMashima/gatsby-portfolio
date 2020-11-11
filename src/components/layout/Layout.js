ef421714755f3d545e102a6a594f834e10169716
Mashima <52222646+zimashima@users.noreply.github.com>
2020-11-11 16:17:00 -0500

    readme

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,14 +2,13 @@ import React from "react"
 import "../css/main.min.css"
 import Header from "./Header"
 import Footer from "./Footer"
-import { Container } from "react-bootstrap"
 
 export default function Layout({ children }){
 
     return (
         <>
             <Header />
-                <div className="container-xl pageContent d-flex align-items-center mt-3 mb-3">
+                <div className="container pageContent d-flex align-items-center mt-3 mb-3">
                     <main>{ children }</main>
                 </div>
             <Footer />
--- src/components/subcomponents/ProjectCard.js
+++ src/components/subcomponents/ProjectCard.js
@@ -8,20 +8,21 @@ export default function ProjectPreview({ project, imageData}) {
     return (
         <>
         
-        <div className="project-card mx-auto mb-5">
+        <div className="col-sm-12 col-md-6 col-lg-4 mb-5 text-center">
             <div className="rounded-lg overflow-hidden shadow-lg project-img">
                 <Link to={`/${project.slug}`}>
                     <Img fluid={imageData.node.fluid} alt={`Screen Shot of ${project.title}'s website`} />
                 </Link>
             </div>
-            <h4 style={{fontSize: '22px'}}className="mt-3 mb-3 text-secondary">
+            <h4 style={{fontSize: '20px'}}className="mt-3 mb-3 text-primary">
                 <Link className="project-title-link" to={`/${project.slug}`}>{project.title}</Link>
             </h4>
+            <p>{project.technology.map(el => <span className="badge badge-sm badge-light m-1"> {el} </span>)}</p>
             <p style={{fontSize: '16px'}} className="mb-3">{project.caption}</p>
-            <div className="d-flex justify-content-start">
-                <Button size="sm" className="button mr-2" variant="outline-secondary" href={`http://www.mashimabutton.com/${project.slug}`}>More Info...</Button>
-                <Button size="sm" className="button mr-2" variant="outline-secondary" href={`${project.githubURL}`}>Github</Button>
-                <Button size="sm" className="button mr-2" variant="outline-secondary" href={`${project.url}`}>Demo</Button>
+            <div className="d-flex justify-content-center">
+                <Button size="sm" className="button mr-2" variant="outline-primary" href={`http://www.mashimabutton.com/${project.slug}`}>More Info...</Button>
+                <Button size="sm" className="button mr-2" variant="outline-warning" href={`${project.githubURL}`}>GitHub</Button>
+                <Button size="sm" className="button mr-2" variant="outline-warning" href={`${project.url}`}>Demo</Button>
             </div>
         </div>
         
